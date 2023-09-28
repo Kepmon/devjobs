0bc565c226fd3700b29faf1468b54cdf49c9be12
Monika <monika.kepasa@gmail.com>
2023-09-28 22:27:23 +0200

    added list of jobs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,20 +3,19 @@ import defaultTheme from 'tailwindcss/defaultTheme'
 
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
 		extend: {
 			fontFamily: {
         sans: ["Kumbh Sans", ...defaultTheme.fontFamily.sans],
       },
 			colors: {
-				blue: {
-					800: 'hsl(219, 29%, 10%)',
-					900: 'hsl(219, 29%, 14%)'
-				},
 				gray: {
 					100: 'hsl(244, 246%, 248%)',
 					300: 'hsl(212, 23%, 69%)',
 					500: 'hsl(214, 17%, 51%)',
+					800: 'hsl(219, 29%, 14%)',
+					900: 'hsl(219, 29%, 10%)'
 				},
         violet: {
 					300: 'hsl(235, 82%, 77%)',
@@ -24,12 +23,20 @@ module.exports = {
 				}
       },
 			fontSize: {
-        baseFluid: 'clamp(0.81rem, calc(0.78rem + 0.21vw), 0.94rem)',
-        lgFluid: 'clamp(0.88rem, calc(0.80rem + 0.42vw), 1.13rem)',
-        xlFluid: 'clamp(1.25rem, calc(1.07rem + 1.04vw), 1.88rem)',
-        xxlFluid: 'clamp(1.63rem, calc(1.22rem + 2.29vw), 3.00rem)'
+				xxs: 'clamp(0.6075rem, 0.5965rem + 0.0627vw, 0.64rem)',
+				xs: 'clamp(0.75rem, 0.7078rem + 0.241vw, 0.875rem)',
+        normal: 'clamp(0.875rem, 0.8328rem + 0.241vw, 1rem)',
+        md: 'clamp(1.05rem, 0.9825rem + 0.3855vw, 1.25rem)',
+        lg: 'clamp(1.26rem, 1.158rem + 0.5831vw, 1.5625rem)',
+        xl: 'clamp(1.5119rem, 1.363rem + 0.8506vw, 1.9531rem)',
+        xxl: 'clamp(2.1775rem, 1.8825rem + 1.6855vw, 3.0519rem)'
+      },
+			boxShadow: {
+        card: '0 0 1px hsl(0, 0%, 59%)',
       },
 		},
 	},
-	plugins: [],
+	plugins: [
+		require('@shrutibalasa/tailwind-grid-auto-fit')
+	],
 }
