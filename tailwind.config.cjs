d67eb0aa6a0e83a5338cb0ebfc619b79f82b17cd
Monika <monika.kepasa@gmail.com>
2023-10-06 22:08:49 +0200

    added a few small improvements

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -22,7 +22,6 @@ module.exports = {
 				},
 				gray: {
 					200: 'hsl(0, 0%, 79%)',
-					300: 'hsl(212, 23%, 69%)',
 					500: 'hsl(214, 17%, 51%)',
 					600: 'hsl(214, 4%, 34%)',
 					700: 'hsl(220, 16%, 22%)',
@@ -41,13 +40,11 @@ module.exports = {
 				sans: ["Kumbh Sans", ...defaultTheme.fontFamily.sans],
 			},
 			fontSize: {
-				xxs: 'clamp(0.6075rem, 0.5965rem + 0.0627vw, 0.64rem)',
 				xs: 'clamp(0.75rem, 0.7078rem + 0.241vw, 0.875rem)',
         normal: 'clamp(0.875rem, 0.8328rem + 0.241vw, 1rem)',
         md: 'clamp(1.05rem, 0.9825rem + 0.3855vw, 1.25rem)',
         lg: 'clamp(1.26rem, 1.158rem + 0.5831vw, 1.5625rem)',
-        xl: 'clamp(1.25rem, 0.9583rem + 1.6667vw, 2.25rem);',
-        xxl: 'clamp(2.1775rem, 1.8825rem + 1.6855vw, 3.0519rem)'
+        xl: 'clamp(1.25rem, 0.9583rem + 1.6667vw, 2.25rem)'
       },
 			animation: {
         'fadeIn': 'fadeIn .3s linear forwards',
