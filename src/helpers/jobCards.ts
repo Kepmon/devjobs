caef62cbf8ce322d072f710ccbffe78ed28b4085
Monika <monika.kepasa@gmail.com>
2023-10-26 00:47:04 +0200

    refactored rendering-new-job-card-related code

--- src/helpers/jobCards.ts
+++ src/helpers/jobCards.ts
@@ -1,46 +1,76 @@
 import type { Job } from "../types/jobs"
 import { returnTimeDifference } from "./timeDifference"
 
+const returnJobStaticData = (job: Job) => ([
+  {
+    selector: 'logo',
+    property: 'src',
+    value: job.logo.at(0)?.url
+  },
+  {
+    selector: 'first-gray-p',
+    property: 'textContent',
+    value: returnTimeDifference(job.xata.createdAt)
+  },
+  {
+    selector: 'third-gray-p',
+    property: 'textContent',
+    value: job.contract
+  },
+  {
+    selector: 'heading',
+    property: 'textContent',
+    value: job.position
+  },
+  {
+    selector: 'company',
+    property: 'textContent',
+    value: job.company
+  },
+  {
+    selector: 'location',
+    property: 'textContent',
+    value: job.location
+  }
+])
+
 const setElementProperty = <T extends HTMLElement>(element: null | T, property: keyof T, value: T[keyof T]) => {
   if (element == null) return
   
   element[property] = value
 }
 
+const renderJobCard = (job: Job, jobsContainer: null | HTMLDivElement, newJobTemplate: null | HTMLTemplateElement) => {
+  const newJobContent = newJobTemplate?.content.cloneNode(true) as null | DocumentFragment
+  const cardLink = newJobContent?.querySelector('[data-template="link"]') as null | HTMLAnchorElement
+  const cardLogoContainer = cardLink?.querySelector('.card-logo') as null | HTMLDivElement
+
+  if (cardLink == null) return
+
+  const jobStaticData = returnJobStaticData(job)
+  jobStaticData.forEach((pieceOfData) => {
+    const htmlElement = cardLink.querySelector(`[data-template="${pieceOfData.selector}"]`) as null | HTMLElement
+
+    setElementProperty(cardLink, 'href', `job/${job.id}`)
+    setElementProperty(htmlElement, (pieceOfData.property as keyof HTMLElement), pieceOfData.value)
+  })
+
+  if (cardLogoContainer != null) {
+    cardLogoContainer.style.backgroundColor = job.logoBackground
+  }
+
+  jobsContainer?.append(cardLink)
+}
+
 export const createNewJobCard = (jobs: Job[], clearContainer?: true) => {
-  const jobsContainer = document.querySelector('[data-container="jobs"]')
+  const jobsContainer = document.querySelector('[data-container="jobs"]') as null | HTMLDivElement
   const newJobTemplate = document.querySelector('[data-template="new-job"]') as null | HTMLTemplateElement
-  
+
   if (clearContainer) {
     jobsContainer?.replaceChildren()
   }
   
   jobs.forEach((job) => {
-    const newJobContent = newJobTemplate?.content.cloneNode(true) as null | DocumentFragment
-    const cardLink = newJobContent?.querySelector('[data-link]') as null | HTMLAnchorElement
-
-    if (cardLink == null) return
-
-    const cardLogoContainer = cardLink.querySelector('.card-logo') as null | HTMLDivElement
-    const cardLogoImg = cardLink.querySelector('[alt="company logo"]') as null | HTMLImageElement
-    const datePosted = cardLink.querySelector('[data-wrapper="gray-text"] p:first-child') as null | HTMLParagraphElement
-    const contract = cardLink.querySelector('[data-wrapper="gray-text"] p:nth-child(3)') as null | HTMLParagraphElement
-    const jobHeading = cardLink.querySelector('[data-heading]') as null | HTMLHeadingElement
-    const company = cardLink.querySelector('[data-company]') as null | HTMLParagraphElement
-    const location = cardLink.querySelector('[data-location]') as null | HTMLParagraphElement
-
-    setElementProperty(cardLink, 'href', `job/${job.id}`)
-    setElementProperty(cardLogoImg, 'src', job.logo.at(0)?.url)
-    setElementProperty(datePosted, 'textContent', returnTimeDifference(job.xata.createdAt))
-    setElementProperty(contract, 'textContent', job.contract)
-    setElementProperty(jobHeading, 'textContent', job.position)
-    setElementProperty(company,'textContent',  job.company)
-    setElementProperty(location, 'textContent', job.location)
-
-    if (cardLogoContainer != null) {
-      cardLogoContainer.style.backgroundColor = job.logoBackground
-    }
-
-    jobsContainer?.append(cardLink)
+    renderJobCard(job, jobsContainer, newJobTemplate)
   })
 }
